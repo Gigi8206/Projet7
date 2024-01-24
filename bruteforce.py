3f686b243d7888568b5fc84402d2544d87242a4b
Hadjer Attatba <archiger@yahoo.fr>
2024-01-24 15:08:40 +0100

    update 24/01

--- bruteforce.py
+++ bruteforce.py
@@ -1,15 +1,14 @@
 from itertools import combinations
-# Fonction pour lire les données à partir du fichier et les stocker dans une liste de tuples
+#lire les données à partir du fichier et les stocker
 def lire_actions(fichier):
     actions = []
     with open(fichier, 'r') as file:
-        next(file)  # Ignorer la première ligne (en-tête)
         for ligne in file:
             action, cout, benefice = ligne.strip().split('\t')
             actions.append((action, int(cout), float(benefice)))
     return actions
 
-# Fonction principale pour trouver la meilleure combinaison d'actions
+# trouver la meilleure combinaison d'actions
 def maximiser_benefices(actions, budget_max):
     meilleure_combinaison = None
     meilleur_profit = 0
@@ -17,4 +16,5 @@ def maximiser_benefices(actions, budget_max):
 # Générer toutes les combinaisons possibles d'actions
     for taille_combinaison in range(1, len(actions) + 1):
         combinaisons = combinations(actions, taille_combinaison)
-
+          # Parcourir chaque combinaison
+        for combinaison in combinaisons:
