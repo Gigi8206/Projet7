5a806ac0b3b8b5dfaae7f0a8c84726a6a4fe4f81
Aurelien lebray <aurelien.lebray@gmail.com>
2024-03-03 17:11:48 +0100

    Push correction

--- bruteforce.py
+++ bruteforce.py
@@ -13,7 +13,7 @@ def lire_actions(fichier):
 def maximiser_benefices(actions, budget_max):
     meilleure_combinaison = None
     meilleur_profit = 0
-    
+
     for taille_combinaison in range(1, len(actions) + 1):
         combinaisons = combinations(actions, taille_combinaison)
 
